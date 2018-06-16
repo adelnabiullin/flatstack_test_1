3ce46c78ef5fec37aeea2e5fa3547d4ac6d8bb9b
Adel Nabiullin <throughyourhead@gmail.com>
2018-06-16 22:11:42 +0300

    Remove old files

--- test_solution.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'strange_sequence_handler'
-
-print 'Type sequence\'s elements count to generate (>1): '
-
-elems_count = gets.chomp.to_i
-
-StrangeSequenceHandler.print_sequence(elems_count)
